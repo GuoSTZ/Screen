47871fedf3769a250134920784aa9a1d63f2e5ce
guosheng <guosheng@mchz.com.cn>
2022-02-07 11:39:30 +0800

    feat: 框架搭建（目前不支持less）

---
+++ src/modules/demo/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import "./index.less";
+
+export default class DemoView extends React.Component<any, any> {
+  render(): React.ReactNode {
+    return (
+      <div className='aaa'>66666</div>
+    )
+  }
+}
